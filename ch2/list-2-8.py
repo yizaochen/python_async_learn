6ee3cd352e54e6e4ebb562cca733198916604f6a
yizaochen <yizaochen@gmail.com>
2024-08-25 23:50:47 +0800

    2-9

---
+++ ch2/list-2-8.py
@@ -0,0 +1,12 @@
+import asyncio
+from util.delay_functions import delay
+
+
+async def main():
+    sleep_for_three = asyncio.create_task(delay(3))
+    print(type(sleep_for_three))
+    result = await sleep_for_three
+    print(result)
+
+
+asyncio.run(main())
